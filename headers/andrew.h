8e7f725a0bd2134cf584b05000b9af0b2b95c5cc
Andrew-Hany <andrewhany@aucegypt.edu>
2020-04-30 19:31:03 +0200

    Andrewhany

---
+++ headers/andrew.h
@@ -0,0 +1,13 @@
+
+#ifndef ANDREW_H_
+#define ANDREW_H_
+
+#include <iostream>
+
+using namespace std;
+
+void andrew();
+
+
+
+#endif 
---
+++ sources/andrew.cpp
@@ -0,0 +1,9 @@
+#include"andrew.h"
+
+void andrew(){
+
+
+cout<<"Andrew Nady"<<endl<<"900184042"<<"andrewhany@aucegypt.edu"<<endl;
+
+
+}
